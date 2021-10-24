803dd5e105fc7acb82ed835f0cb7752957c21a39
Ajani Lewis <ajani.j.lewis@gmail.com>
2021-10-24 14:56:47 -0500

    Edited scripts.js, added functionality to subscribe buttton

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
-/* Add your JavaScript to this file */
+window.onload = function(){
+    let button = document.getElementsByTagName("form")[0].getElementsByTagName("button")[0];
+    let message = document.getElementsByClassName("message")[0];
+    let email = document.getElementById("email");
+
+    console.log(email);
+
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        let messages = [];
+
+        if(email.value ==="" || email.value == null){
+            messages.push("Please Enter a Valid Email Address");
+
+        }else if(email.value.indexOf("@") == -1){
+            messages.push("Please Enter a Valid Email Address");
+
+        }else{
+            messages.push("Thank You! Your email address " + email.value + " has been added to our mailing list!");
+        }
+        
+        message.innerHTML = messages[0];
+
+    });
+
+};
